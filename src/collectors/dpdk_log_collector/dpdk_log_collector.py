890966d4227b779641f70595d233b132df17a794
yuchenyao <yaoyuchen0626@163.com>
2023-03-13 14:58:38 +0800

    refactor dpdklog
    Signed-off-by: yuchenyao <yaoyuchen0626@163.com>

--- src/collectors/dpdk_log_collector/dpdk_log_collector.py
+++ src/collectors/dpdk_log_collector/dpdk_log_collector.py
@@ -7,17 +7,16 @@ The DpdkLogCollector uses the socket to receive dpdk logs.
 import diamond.collector
 import socket
 
+
 class DpdkLogCollector(diamond.collector.Collector):
 
     def process_config(self):
         super(DpdkLogCollector, self).process_config()
         self.hostname = self.config['hostname']
-        self.port = self.config['port']
-        self.server_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       
+        self.port = int(self.config['port'])
 
     def get_default_config_help(self):
-        config_help =  super(DpdkLogCollector, self).get_default_config_help()
+        config_help = super(DpdkLogCollector, self).get_default_config_help()
         config_help.update({
             'hostname': 'Hostname',
             'port': 'Port',
@@ -37,14 +36,37 @@ class DpdkLogCollector(diamond.collector.Collector):
         })
         return config
 
-    def collect(self ):
-        # TODO: should connect and close in this function? 
-        self.server_client.connect((self.hostname, self.port)) 
-       
-        recv_str = self.server_client.recv(128)
-        recv_str = recv_str.decode("ascii")
-        print("receive:{}".format(recv_str))
+    def collect(self):
+        recv_str = ""
+        try:
 
+            self.server_client = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+            self.server_client.connect((self.hostname, self.port))
+
+            self.log.info("connected")
+
+#               self.server_client.settimeout(3)
+
+            while True:
+                rec = self.server_client.recv(128)
+                if not rec:
+                    break
+
+                recv_str += rec
+
+            recv_str = recv_str.decode("ascii")
+
+            self.log.info("receive:{}".format(str(recv_str).split()))
+        except socket.info:
+            self.log.exception('Failed to get stats from %s:%s',
+                               self.hostname, self.port)
+
+        self.log.info("closing")
+        self.server_client.close()
+
+        self.log.info("publishing...")
         # publish numbers
-        self.publish("numbers:", str(recv_str).split( )[-1])
+        self.publish("dpdk_arg:", str(recv_str).split()[0])
 
+        self.log.info("done")
